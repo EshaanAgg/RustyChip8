276ebe0e37354d1a9135ffa158a5ea5c122b7bb5
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-03-31 02:03:42 +0530

    Add complete desktop code

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,14 +1,21 @@
 use chip8::*;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 
-// SCALING PREFERENCES so that the game appears properly on the Emulation
+// STYLISTIC PREFERENCES so that the game appears properly on the Emulation
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
+// Main code
 fn main() {
     let args: Vec<_> = env::args().collect();
     // Just accept 1 argument
@@ -48,15 +55,91 @@ fn main() {
         // Listen for all possible events
         for evt in event_pump.poll_iter() {
             match evt {
-                // If the window is closed, exit out of the game by ending the gameloop
-                Event::Quit { .. } => {
+                // If the window is closed or the ESCAPE button is pressed, exit out of the game by ending the gameloop
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'gameloop;
                 }
+
+                // If a key is pressed, set the same to be pressed in the CPU
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                }
+
+                // If a key is released, set the same to be released in the CPU
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
+                }
+
+                // Handle all the rest of the events, and do nothing
                 _ => (),
             }
         }
 
         // Execute a clock-cycle
-        chip8.tick();
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
+
+        // Draw the screen
+        draw_screen(&chip8, &mut canvas);
+    }
+}
+
+// Draw's the canvas
+fn draw_screen(chip8: &CPU, canvas: &mut Canvas<Window>) {
+    // Clear canvas as black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = chip8.get_display();
+    // Now set draw color to white, iterate through each point and see if it should be drawn
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            // Convert our 1D array's index into a 2D (x,y) position
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            // Draw a rectangle at (x,y), scaled up by our SCALE value
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
+}
+
+// Converts a keypress on the actual device to a byte KEY for the emulator
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
     }
 }
